e025d0c2e21ed80dce3d6d7b0cc5ceae45d59cee
Shougotmollik <Shougotmollik@gmail.com>
2023-03-23 01:45:52 +0600

    first commit

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+#string concatenation (how to put strings together)
+# f string doing the job very pretty .. !!!
+
+adjective=input("Adjective : ")
+verb1=input("verb :")
+verb2=input("verb :")
+famous_person=input(" Famous person: ")
+
+madlib=f"computer programming is so {adjective}! It makes me very happy all the time when seat to code something. \
+I love to {verb1}.Stay cool and drink water also {verb2} like you are {famous_person}"
+
+print(madlib)
